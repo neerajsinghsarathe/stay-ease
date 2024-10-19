ac6ba68e027b463bd531ca8d4bd28e04a2666a5d
Neeraj Singh Sarathe <neerajssarathe@gmail.com>
2024-10-20 00:21:31 +0530

    Added spinner component

--- stay-ease-angular/src/app/app.component.ts
+++ stay-ease-angular/src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './helpers/header/header.component';
 import {FooterComponent} from './helpers/footer/footer.component';
+import {SpinnerComponent} from './helpers/spinner/spinner.component';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent, SpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
